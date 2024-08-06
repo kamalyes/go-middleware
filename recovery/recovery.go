f4d6609e346c9ec1054c5ff269922051ac157806
kamalyes <501893067@qq.com>
2024-08-06 11:08:26 +0800

    :fire: feat: 优化recovery组件日志明细

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -15,8 +15,8 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"runtime/debug"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kamalyes/go-core/global"
@@ -26,48 +26,47 @@ import (
 // GinRecoveryMiddleware 用于recover可能出现的panic，并使用zap记录相关日志
 func GinRecoveryMiddleware(stack bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		//延迟处理的函数
 		defer func() {
+			// 发生宕机时，获取panic传递的上下文并打印
 			if err := recover(); err != nil {
-				// 检查错误类型
+				// 获取用户的请求信息
+				httpRequest, _ := httputil.DumpRequest(c.Request, true)
+				// 链接中断，客户端中断连接为正常行为，不需要记录堆栈信息
 				var brokenPipe bool
 				if ne, ok := err.(*net.OpError); ok {
 					if se, ok := ne.Err.(*os.SyscallError); ok {
-						errorMsg := strings.ToLower(se.Error())
-						if strings.Contains(errorMsg, "broken pipe") || strings.Contains(errorMsg, "connection reset by peer") {
+						errStr := strings.ToLower(se.Error())
+						if strings.Contains(errStr, "broken pipe") || strings.Contains(errStr, "connection reset by peer") {
 							brokenPipe = true
 						}
 					}
 				}
-
-				// 获取请求信息
-				httpRequest, _ := httputil.DumpRequest(c.Request, false)
-
-				// 错误处理
+				// 链接中断的情况
 				if brokenPipe {
-					global.LOG.Error("Broken pipe or connection reset by peer",
+					global.LOG.Error(c.Request.URL.Path,
+						zap.String("request_id", c.GetString("request_id")), //用于链路追踪使用
+						zap.Time("time", time.Now()),
 						zap.Any("error", err),
 						zap.String("request", string(httpRequest)),
 					)
-					_ = c.Error(err.(error))
+					c.Error(err.(error))
 					c.Abort()
+					// 链接已断开，无法写状态码
 					return
 				}
-
-				// 日志记录
-				logFields := []zap.Field{
-					zap.Any("error", err),
-					zap.String("request", string(httpRequest)),
-				}
-				if stack {
-					logFields = append(logFields, zap.String("stack", string(debug.Stack())))
-				}
-
-				global.LOG.Error("Recovery from panic",
-					logFields...,
+				// 如果不是链接中断，就开始记录堆栈信息
+				global.LOG.Error("recovery from panic",
+					zap.String("request_id", c.GetString("request_id")), //用于链路追踪使用
+					zap.Time("time", time.Now()),                        //记录时间
+					zap.Any("error", err),                               //记录错误信息
+					zap.String("request", string(httpRequest)),          //请求信息
+					zap.Stack("stacktrace"),                             //调用堆栈信息
 				)
-
-				// 返回500错误
-				c.AbortWithStatus(http.StatusInternalServerError)
+				// 返回 500 状态码
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+					"message": "服务器内部错误，请稍后再试",
+				})
 			}
 		}()
 		c.Next()
---
+++ recovery/recovery_test.go
@@ -0,0 +1,51 @@
+/*
+ * @Author: kamalyes 501893067@qq.com
+ * @Date: 2023-07-28 00:50:58
+ * @LastEditors: kamalyes 501893067@qq.com
+ * @LastEditTime: 2024-08-06 10:37:57
+ * @FilePath: \go-middleware\recovery\recovery_test.go
+ * @Description:
+ *
+ * Copyright (c) 2024 by kamalyes, All Rights Reserved.
+ */
+package recovery
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kamalyes/go-core/global"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGinRecoveryMiddleware(t *testing.T) {
+	global.InitZapLogger()
+	gin.SetMode(gin.TestMode)
+
+	router := gin.New()
+	router.Use(GinRecoveryMiddleware(true))
+
+	router.GET("/ok", func(c *gin.Context) {
+		// 返回 200 状态码
+		c.AbortWithStatus(200)
+	})
+
+	okRecorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ok", nil)
+	router.ServeHTTP(okRecorder, req)
+	assert.Equal(t, http.StatusOK, okRecorder.Code, "Status code is 200")
+
+	router.GET("/panic", func(c *gin.Context) {
+		panic("test panic")
+	})
+
+	panicRecorder := httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/panic", nil)
+	router.ServeHTTP(panicRecorder, req)
+
+	assert.Equal(t, http.StatusInternalServerError, panicRecorder.Code, "Status code is not 500")
+	expectedResponse := "{\"message\":\"服务器内部错误，请稍后再试\"}"
+	assert.Contains(t, panicRecorder.Body.String(), expectedResponse, "Response body does not contain expected message")
+}
